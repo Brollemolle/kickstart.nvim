2752ae99f34f1163e3f7c75721c297c2468a3976
Brollemolle <brolinadam5@gmail.com>
2025-02-11 15:56:28 +0100

    New plugins.

--- init.lua
+++ init.lua
@@ -733,7 +733,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = false, cpp = false, java = false }
+        local disable_filetypes = { c = false, cpp = false, java = true }
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
           lsp_format_opt = 'never'
