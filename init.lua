6f776c8f3e100cf3cc16f91310d0505d2431b2c6
Adam Brolin <adam@brolin.email>
2025-11-08 12:43:53 +0100

    remove nix binaries for LSPs

--- init.lua
+++ init.lua
@@ -641,73 +641,73 @@ require('lazy').setup({
       -- ... other configuration options ...
       --}
 
-      require('lspconfig').rust_analyzer.setup {
-        cmd = { 'rust-analyzer' }, -- Uses Nix's binary
-        settings = {
-          ['rust-analyzer'] = {
-            checkOnSave = { command = 'clippy' }, -- Optional: Enable clippy
-          },
-        },
-      }
-
-      require('lspconfig').clangd.setup {
-        cmd = { 'clangd' }, -- Uses Nix's binary
-      }
-
-      require('lspconfig').jdtls.setup {
-        cmd = { 'jdtls' }, -- Uses Nix's binary
-      }
-
-      require('lspconfig').tinymist.setup {
-        cmd = { 'tinymist' }, -- Uses Nix's binary
-      }
-
-      require('lspconfig').nixd.setup {
-        cmd = { 'nixd' }, -- Uses Nix's binary
-      }
-
-      require('lspconfig').elp.setup {
-        cmd = { 'elp' }, -- Uses Nix's binary
-      }
-
-      require('lspconfig').erlangls.setup {
-        cmd = { 'erlang_ls' }, -- Uses Nix's binary
-      }
-
-      require('lspconfig').ts_ls.setup {
-        cmd = { 'typescript-language-server', '--stdio' }, -- Uses Nix's binary
-      }
-
-      require('lspconfig').ltex.setup {
-        cmd = { 'ltex-ls' },
-        settings = {
-          ltex = {
-            -- enabled = { 'typst' },
-            language = 'en-US',
-            additionalRules = {
-              languageModel = '~/models/ngrams/',
-            },
-          },
-        },
-        -- on_attach = handlers.on_attach,
-        -- capabilities = capabilities,
-        -- settings = {
-        --   ltex = {
-        --     enabled = { 'typst' }, -- Specify Typst as the file type
-        --     language = 'en-US', -- Set the language for spell checking
-        --     diagnosticSeverity = 'info',
-        --     checkFrequency = 'save',
-        --     sentenceCacheSize = 2000,
-        --     additionalRules = {
-        --       enablePickyRules = false,
-        --     },
-        --     trace = { server = 'verbose' },
-        --     disabledRules = {
-        --       ['en-US'] = { 'MORFOLOGIK_RULE_EN_US' },
-        --     },
-        --   },
-        -- },
-      }
+      -- require('lspconfig').rust_analyzer.setup {
+      --   cmd = { 'rust-analyzer' }, -- Uses Nix's binary
+      --   settings = {
+      --     ['rust-analyzer'] = {
+      --       checkOnSave = { command = 'clippy' }, -- Optional: Enable clippy
+      --     },
+      --   },
+      -- }
+      --
+      -- require('lspconfig').clangd.setup {
+      --   cmd = { 'clangd' }, -- Uses Nix's binary
+      -- }
+      --
+      -- require('lspconfig').jdtls.setup {
+      --   cmd = { 'jdtls' }, -- Uses Nix's binary
+      -- }
+      --
+      -- require('lspconfig').tinymist.setup {
+      --   cmd = { 'tinymist' }, -- Uses Nix's binary
+      -- }
+      --
+      -- require('lspconfig').nixd.setup {
+      --   cmd = { 'nixd' }, -- Uses Nix's binary
+      -- }
+      --
+      -- require('lspconfig').elp.setup {
+      --   cmd = { 'elp' }, -- Uses Nix's binary
+      -- }
+      --
+      -- require('lspconfig').erlangls.setup {
+      --   cmd = { 'erlang_ls' }, -- Uses Nix's binary
+      -- }
+      --
+      -- require('lspconfig').ts_ls.setup {
+      --   cmd = { 'typescript-language-server', '--stdio' }, -- Uses Nix's binary
+      -- }
+      --
+      -- require('lspconfig').ltex.setup {
+      --   cmd = { 'ltex-ls' },
+      --   settings = {
+      --     ltex = {
+      --       -- enabled = { 'typst' },
+      --       language = 'en-US',
+      --       additionalRules = {
+      --         languageModel = '~/models/ngrams/',
+      --       },
+      --     },
+      --   },
+      --   -- on_attach = handlers.on_attach,
+      --   -- capabilities = capabilities,
+      --   -- settings = {
+      --   --   ltex = {
+      --   --     enabled = { 'typst' }, -- Specify Typst as the file type
+      --   --     language = 'en-US', -- Set the language for spell checking
+      --   --     diagnosticSeverity = 'info',
+      --   --     checkFrequency = 'save',
+      --   --     sentenceCacheSize = 2000,
+      --   --     additionalRules = {
+      --   --       enablePickyRules = false,
+      --   --     },
+      --   --     trace = { server = 'verbose' },
+      --   --     disabledRules = {
+      --   --       ['en-US'] = { 'MORFOLOGIK_RULE_EN_US' },
+      --   --     },
+      --   --   },
+      --   -- },
+      -- }
 
       -- require('lspconfig').lua_language_server.setup {
       --   cmd = { 'lua-language-server' },
@@ -725,38 +725,45 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         sqlls = {},
-        -- jdtls = {},
+        jdtls = {},
         -- java_language_server = {},
-        -- gopls = {},
+        gopls = {},
         pyright = {},
+        clangd = {},
+        rust_analyzer = {},
+        ts_ls = {},
         -- rnix = {},
         -- ltex_ls = {},
-        -- rust_analyzer = {
-        --   cmd = { "rust-analyzer" },
-        -- },
+        rust_analyzer = {
+          cmd = { "rust-analyzer" },
+          settings = {
+            ['rust-analyzer'] = {
+              checkOnSave = { command = 'clippy' }, -- Optional: Enable clippy
+            },
+        },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
         --
 
-        -- lua_ls = {
-        --   -- cmd = {...},
-        --   -- filetypes = { ...},
-        --   -- capabilities = {},
-        --   settings = {
-        --     Lua = {
-        --       completion = {
-        --         callSnippet = 'Replace',
-        --       },
-        --       -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-        --       -- diagnostics = { disable = { 'missing-fields' } },
-        --     },
-        --   },
-        -- },
+        lua_ls = {
+          -- cmd = {...},
+          -- filetypes = { ...},
+          -- capabilities = {},
+          settings = {
+            Lua = {
+              completion = {
+                callSnippet = 'Replace',
+              },
+              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+              -- diagnostics = { disable = { 'missing-fields' } },
+            },
+          },
+        },
       }
 
       -- Ensure the servers and tools above are installed
